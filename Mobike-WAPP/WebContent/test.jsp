e5300df99c707089e4843f47c122289a81c15add
thespeedfreak <bvispi@gmail.com>
2015-01-07 17:35:58 +0100

    Added the javascript code for formatting route json in a table. Additional css and html fixes

--- Mobike-WAPP/WebContent/test.jsp
+++ Mobike-WAPP/WebContent/test.jsp
@@ -5,28 +5,78 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Insert title here</title>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>
+    	 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    	<!--  <script>
 
 	$(document).ready(function(){
 		  $("button").click(function(){
-			  var urir = "http://localhost:8080/Mobike-SRV/routes/retrieveall"
+			  var urir = "http://localhost:8080/Mobike-WAPP/testjson";
 		    $.getJSON(urir,function(result){
 		      $.each(result, function(i, field){
+		    	  $('#tabella').append("<tr>"+field+"</td>");
 		    	  $.each(field, function(k, v){
-		    		  $("div").append(v + " ");
+		    		  $('#tabella').append("<td>"+ v +"</td>");
 		    	  });
 		       
 		      });
 		    });
 		  });
 		});
-</script>
+	</script>
+	-->
+	<script>
+	
+	$.makeTable = function (mydata) {
+	    var table = $('<table border=1>');
+	    var tblHeader = "<tr>";
+	    for (var k in mydata[0]) tblHeader += "<th>" + k + "</th>";
+	    tblHeader += "</tr>";
+	    $(tblHeader).appendTo(table);
+	    $.each(mydata, function (index, value) {
+	        var TableRow = "<tr>";
+	        $.each(value, function (key, val) {
+	            TableRow += "<td>" + val + "</td>";
+	        });
+	        TableRow += "</tr>";
+	        $(table).append(TableRow);
+	    });
+	    return ($(table));
+	};
+	
+	
+	
+	$(document).ready(function(){
+			  var urir = "http://localhost:8080/Mobike-WAPP/testjson";
+		    $.getJSON(urir,function(result){
+	var mydata = result;
+	var table = $.makeTable(mydata);
+	$(table).appendTo("#ciao");
+		    });
+		  
+	});
+		    
+	</script>
+	
+
 </head>
 <body>
+<table id="tabella">
+<tr>
+<td> uno </td>
+<td> due </td>
+<td> tre </td>
+<td> quattro </td> </tr>
+<tr>
+<td> 1 </td>
+<td> 2 </td>
+<td> 3 </td>
+<td> 4 </td> </tr>
+
+
+</table>
 
 <button>Get JSON data</button>
-<div></div>
+<div id="ciao"></div>
 
 </body>
 </html>
--- Mobike-WAPP/src/controller_client/ControllerClient.java
+++ Mobike-WAPP/src/controller_client/ControllerClient.java
@@ -26,7 +26,7 @@ import com.sun.jersey.api.view.Viewable;
 @Path("/")
 public class ControllerClient {
 	
-	private static final String BaseURI = "http://localhost:8080/Mobike-SRV/";
+	private static final String BaseURI = "http://mobike.ddns.net/SRV/";
 	
 	Client client = Client.create();
 	private final WebResource wr = client.resource(BaseURI);
@@ -43,107 +43,31 @@ public class ControllerClient {
 	}
 	
 	@GET
-	@Path("/prova")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String prova(){
-		return "helloprova";
+	@Path("/itineraries")
+	public Viewable itineraries(){
+		return new Viewable("/itineraries.html", "itineraries");
 	}
 	
 	@GET
-	@Path("/routes")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Viewable getRoutes(){
-		ClientResponse response = wr.path("/routes").path("/retrieveall").accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-		String output = response.getEntity(String.class);
-		String output2 = output.substring(1, output.length());
-		JSONObject object = null;
-		try {
-			object = new JSONObject(output2);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return new Viewable("/paths.html",object);
-		
-		
-		
-	
+	@Path("/aboutus")
+	public Viewable aboutus(){
+		return new Viewable("/aboutus.html", "aboutus");
 	}
 	
 	@GET
 	@Path("/test")
 	public Viewable testmethod(){
+		System.out.println("test invocato");
 		return new Viewable("/test.jsp","welcome");
 	}
-	/*
+	
 	@GET
-	@Path("/analysis")
-	public Viewable startAnalysis(){
-		return new Viewable("/startAnalysis.jsp", "startsearch");
-	}
-
-	@GET
-	@Path("/references")
-	public Viewable references(){
-		return new Viewable("/references.jsp", "references");
-	}
-	@POST
-	@Path("/search/done")
-	//?brand={brand}&data={data}
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Path("/testjson")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Viewable doSearch(@NotNull @FormParam("brand") String brand,
-							@NotNull @FormParam("until") String untilDate,
-							@NotNull @FormParam("since") String sinceDate){
-			
-		String input = "{\"brand\":\""+brand+"\",\"sinceDate\":\""+sinceDate+"\",\"untilDate\":\""+untilDate+"\"}";
-
-		ClientResponse response = wr.path("/search").path("/start").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, input);
-		JSONObject output = response.getEntity(JSONObject.class);
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String testjson(){
+		String output = wr.path("/routes").path("/retrieveall").accept(MediaType.APPLICATION_JSON).get(String.class);
+		return output;
 		
-		return new Viewable("/analysisDone",output);
 	}
-	
-
-	@GET
-	@Path("/analysis/start")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Viewable doAnalysis(@NotNull @QueryParam("brand") String brand,
-								@NotNull @QueryParam("since") String sinceDate,
-								@NotNull @QueryParam("until") String untilDate){
-			
-		String input = "{\"brand\":\""+brand+"\",\"sinceDate\":\""+sinceDate+"\",\"untilDate\":\""+untilDate+"\"}";
-		System.out.println(input);
-		ClientResponse response = wr.path("/analysis").path("/start").type(MediaType.APPLICATION_JSON).post(ClientResponse.class,input);
-		JSONObject output = response.getEntity(JSONObject.class);
-		
-		return new Viewable("/analysisDone",output);*/
-	}
-	
-	
-	
-//	//TEST
-//	@GET
-//	@Path("/hello")
-//	public Viewable getHello(){
-//		ClientResponse response = wr.path("/hello").accept(MediaType.TEXT_PLAIN).get(ClientResponse.class);
-//		
-//		if (response.getStatus() != 200) {
-//			   throw new RuntimeException("Failed : HTTP error code : "
-//				+ response.getStatus());
-//			}
-//	 
-//			String output = response.getEntity(String.class);
-//	 
-//			System.out.println("Output from Server .... \n");
-//			System.out.println(output);
-//			return new Viewable("/provaoutput", output);
-//	}
-//	
-//	@GET
-//	@Path("/prova/prova")
-//	public Viewable get(){
-//		return new Viewable("/provaoutput", "bello");
-//	}
-//	
+}
