db4d2b09717130aeebabc514877257c8aea1f443
Paolo <pao.calabrese1@stud.uniroma3.it>
2015-03-16 02:19:14 +0100

    added Crypter unit and WIP /connect action flow.

--- Mobike-WAPP/src/controller_client/ControllerClient.java
+++ Mobike-WAPP/src/controller_client/ControllerClient.java
@@ -1,217 +1,356 @@
 package controller_client;
 
-
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.URI;
+import java.security.SecureRandom;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
+import utils.Crypter;
+
+import com.google.api.client.auth.oauth2.TokenResponseException;
+import com.google.api.client.googleapis.auth.oauth2.*;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.oauth2.Oauth2;
+import com.google.api.services.oauth2.model.Tokeninfo;
+import com.google.api.services.oauth2.model.Userinfoplus;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.view.Viewable;
+import com.sun.jndi.toolkit.url.Uri;
 
 @Path("/")
 public class ControllerClient {
 
 	private static final String BaseURI = "http://mobike.ddns.net/SRV/";
-	
+	private final String BaseWebAppUri = "http://mobike.ddns.net/WAPP/";
+
 	Client client = Client.create();
 	private final WebResource wr = client.resource(BaseURI);
-	
+
 	@GET
-	public Viewable welcome(){
+	public Viewable welcome() {
 		return new Viewable("/index.jsp", null);
 	}
-	
+
 	@GET
 	@Path("/landing")
-	public Viewable landing(){
-		return new Viewable("/landing.jsp");
+	public Response landing(@Context HttpServletRequest request) {
+
+		return Response.ok(new Viewable("/landing2.jsp")).build();
 	}
-	
+
 	@GET
 	@Path("/home")
-	public Viewable home(){
+	public Viewable home() {
 		return new Viewable("/index.jsp", null);
 	}
-	
+
 	@GET
 	@Path("/itineraries")
-	public Viewable itineraries(){
+	public Viewable itineraries() {
 		return new Viewable("/itineraries.jsp", null);
 	}
-	
+
 	@GET
 	@Path("/aboutus")
-	public Viewable aboutus(){
+	public Viewable aboutus() {
 		return new Viewable("/aboutus.jsp", null);
 	}
-	
+
 	@GET
 	@Path("/getitineraries")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getItineraries(){
-		String output = wr.path("/routes").path("/retrieveall").accept(MediaType.APPLICATION_JSON).get(String.class);
+	public String getItineraries() {
+		String output = wr.path("/routes").path("/retrieveall")
+				.accept(MediaType.APPLICATION_JSON).get(String.class);
 		return output;
-		
+
 	}
+
 	@GET
 	@Path("/getitnbyid/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getItnById(@PathParam ("id") long id){
-		String output = wr.path("/routes").path("/retrieve").path("/"+id).accept(MediaType.APPLICATION_JSON).get(String.class);
+	public String getItnById(@PathParam("id") long id) {
+		String output = wr.path("/routes").path("/retrieve").path("/" + id)
+				.accept(MediaType.APPLICATION_JSON).get(String.class);
 		return output;
-	
+
 	}
-	
+
 	@GET
 	@Path("/itineraries/{id}")
-	public Viewable itineraryDetails(@PathParam ("id") long id){
+	public Viewable itineraryDetails(@PathParam("id") long id) {
 		return new Viewable("/itinerarydetail.jsp", null);
 	}
-	@GET
-	@Path("/itntest")
-	@Produces("text/html")
-	public Viewable itntest(@Context HttpServletRequest req){
-		HttpSession session = req.getSession(true);
-		
-		Map<String,Integer> map = new HashMap<String,Integer>();
-		map.put("chiave1", 1);
-		map.put("chiave2", 2);
-		map.put("chiave3", 3);
-		session.setAttribute("mappa", map);
-		return new Viewable("/test.jsp",null);
-	}
+
 	
 	@POST
 	@Path("/login")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String login(String json, @Context HttpServletRequest req){
-		//inizializzo Gson
+	public String login(String json, @Context HttpServletRequest req) {
+		// inizializzo Gson
 		Gson gson = new GsonBuilder().create();
-		//Converto il json in un oggetto Properties
+		// Converto il json in un oggetto Properties
 		Properties pr = gson.fromJson(json, Properties.class);
-		
-		
+
 		String id = null;
-		ClientResponse response = wr.path("/users").path("/auth").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, json);
+		ClientResponse response = wr.path("/users").path("/auth")
+				.type(MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class, json);
 		id = response.getEntity(String.class);
-	
-		//Prendo la sessione e aggiungo i dati dell'utente in una HashMap User
+
+		// Prendo la sessione e aggiungo i dati dell'utente in una HashMap User
 		HttpSession session = req.getSession(true);
-		Map <String,String> user = new HashMap<String,String>();
+		Map<String, String> user = new HashMap<String, String>();
 		user.put("name", pr.getProperty("name"));
 		user.put("surname", pr.getProperty("surname"));
 		user.put("email", pr.getProperty("email"));
-		if(id!=null)
+		if (id != null)
 			user.put("id", id);
-		
-		//setto la HashMap come attributo della sessione
+
+		// setto la HashMap come attributo della sessione
 		session.setAttribute("user", user);
-		
-		
-		
-		//restituisco la URL a cui reindirizzare il browser
+
+		// restituisco la URL a cui reindirizzare il browser
 		return "/WAPP/home";
 	}
 
-	
 	@GET
 	@Path("/logout")
 	@Consumes(MediaType.TEXT_PLAIN)
-	public String logout(@Context HttpServletRequest req){
+	public String logout(@Context HttpServletRequest req) {
 		HttpSession session = req.getSession();
-		
-		if(session != null){
+
+		if (session != null) {
 			session.removeAttribute("user");
 		}
-		
-		return "/WAPP/landing";	
+
+		return "/WAPP/landing";
 	}
-	
+
 	@GET
 	@Path("/getEvents")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getEvents(){
-		String output = wr.path("/events").path("/retrieveall").accept(MediaType.APPLICATION_JSON).get(String.class);
+	public String getEvents() {
+		String output = wr.path("/events").path("/retrieveall")
+				.accept(MediaType.APPLICATION_JSON).get(String.class);
 		return output;
 	}
-	
+
 	@GET
 	@Path("/getEventById/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-		public String getEventById(@PathParam ("id") long id){
-			String output = wr.path("/events").path("/retrieve").path("/"+id).accept(MediaType.APPLICATION_JSON).get(String.class);
-			return output;
-		
-		}
-	
+	public String getEventById(@PathParam("id") long id) {
+		String output = wr.path("/events").path("/retrieve").path("/" + id)
+				.accept(MediaType.APPLICATION_JSON).get(String.class);
+		return output;
+
+	}
+
 	@GET
 	@Path("/events")
 	@Produces(MediaType.TEXT_HTML)
-	public Viewable events(){
-		return new Viewable("/events.jsp",null);
+	public Viewable events() {
+		return new Viewable("/events.jsp", null);
 	}
-	
+
 	@GET
 	@Path("/events/{id}")
 	@Produces("text/html")
-	public Response eventId(@PathParam("id") long id){
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("id",String.valueOf(id));
-		return Response.ok(new Viewable("/eventdetail.jsp",map)).build();
+	public Response eventId(@PathParam("id") long id) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("id", String.valueOf(id));
+		return Response.ok(new Viewable("/eventdetail.jsp", map)).build();
 	}
-	
+
 	@GET
 	@Path("/events/new")
 	@Produces(MediaType.TEXT_HTML)
-	public Viewable eventnew(){
-		return new Viewable("/createevent.jsp",null);
+	public Viewable eventnew() {
+		return new Viewable("/createevent.jsp", null);
 	}
-	
+
 	@POST
 	@Path("/events/insertnew")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("text/html")
-	public Response insertNew(@FormParam("name")String name, @FormParam("description") String description, 
-							@FormParam("date") String date, @FormParam("time") String time, 
-							@FormParam("startLocation") String startLocation, @FormParam("routeId") String routeId,@Context HttpServletRequest req){
-		
+	public Response insertNew(@FormParam("name") String name,
+			@FormParam("description") String description,
+			@FormParam("date") String date, @FormParam("time") String time,
+			@FormParam("startLocation") String startLocation,
+			@FormParam("routeId") String routeId,
+			@Context HttpServletRequest req) {
+
 		HttpSession session = req.getSession();
 		@SuppressWarnings("unchecked")
-		Map<String,String> user = (Map<String, String>) session.getAttribute("user");
-		String creatorId  = user.get("id");
+		Map<String, String> user = (Map<String, String>) session
+				.getAttribute("user");
+		String creatorId = user.get("id");
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date currentDate = new Date();
 		String creationDate = dateFormat.format(currentDate);
 		String dateTime = date + " " + time + ":00";
-		
-		String json = "{\"creatorId\"=\"" + creatorId + "\", \"name\"=\"" + name + "\", \"description\"=\"" + description + "\", \"routeId\"=\"" + routeId +"\", \"startDate\"=\"" + dateTime +
-				"\", \"startLocation\"=\"" + startLocation +"\", \"creationDate\"=\"" + creationDate + "\"}";
-		
-		ClientResponse response = wr.path("/events").path("/create").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, json);
+
+		String json = "{\"creatorId\"=\"" + creatorId + "\", \"name\"=\""
+				+ name + "\", \"description\"=\"" + description
+				+ "\", \"routeId\"=\"" + routeId + "\", \"startDate\"=\""
+				+ dateTime + "\", \"startLocation\"=\"" + startLocation
+				+ "\", \"creationDate\"=\"" + creationDate + "\"}";
+
+		ClientResponse response = wr.path("/events").path("/create")
+				.type(MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class, json);
 		String output = response.getEntity(String.class);
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("id",output);
-		return Response.ok(new Viewable("/eventdetail.jsp",map)).build();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("id", output);
+		return Response.ok(new Viewable("/eventdetail.jsp", map)).build();
 	}
+
+	@POST
+	@Path("/connect")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	public Response connectServer(String x, @CookieParam("state") String state,
+			@CookieParam("token") String userToken,
+			@Context HttpServletRequest request) {
+
+		// check if user is logged, check if it's authorization is valid and
+		// then redirect browser.
+		if (userToken != null) {
+			System.out.println("entered 1st if");
+			ClientResponse response = wr.path("/users").path("/auth")
+					.type(MediaType.APPLICATION_JSON)
+					.post(ClientResponse.class, userToken);
+
+			if (response.getStatus() == 200)
+				return Response.ok(this.BaseWebAppUri.concat("home")).build();
+			else {
+				return Response.ok(this.BaseWebAppUri.concat("landing"))
+						.build();
+			}
+
+		}
+		// initiate the Google Authentication Flow.
+		else {
+			String code = x;
+			HttpTransport TRANSPORT = new NetHttpTransport();
+			JacksonFactory JSON_FACTORY = new JacksonFactory();
+			String CLIENT_ID = "648355147327-l29pdutihnfa25kdmo1ocjankqg4217c.apps.googleusercontent.com";
+			String CLIENT_SECRET = "kFPvMH88ifFkbKYnsFsPq5Me";
+			String name, surname, email, imgUrl = null;
+
+			try {
+				List<String> scopes = new ArrayList<String>();
+				scopes.add("https://www.googleapis.com/auth/plus.login");
+				scopes.add("https://www.googleapis.com/auth/plus.me");
+				scopes.add("https://www.googleapis.com/auth/userinfo.profile");
+				scopes.add("https://www.googleapis.com/auth/userinfo.email");
+				scopes.add("https://www.googleapis.com/auth/plus.profile.emails.read");
+				GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
+						TRANSPORT, JSON_FACTORY, CLIENT_ID, CLIENT_SECRET,
+						code, "postmessage").setScopes(scopes).execute();
+				GoogleCredential credential = new GoogleCredential.Builder()
+						.setJsonFactory(JSON_FACTORY).setTransport(TRANSPORT)
+						.setClientSecrets(CLIENT_ID, CLIENT_SECRET).build()
+						.setFromTokenResponse(tokenResponse);
+
+				Oauth2 oauth2 = new Oauth2.Builder(TRANSPORT, JSON_FACTORY,
+						credential).setApplicationName("mobike").build();
+
+				@SuppressWarnings("unused")
+				Tokeninfo tokenInfo = oauth2.tokeninfo()
+						.setAccessToken(credential.getAccessToken()).execute();
+
+				Userinfoplus userinfo = oauth2.userinfo().get().execute();
+
+				name = userinfo.getGivenName();
+				surname = userinfo.getFamilyName();
+				email = userinfo.getEmail();
+				imgUrl = userinfo.getPicture();
+			} catch (Exception e) {
+				return Response.status(500).entity(e.getMessage()).build();
+			}
+
+			Map<String, String> userMap = new HashMap<String, String>();
+			userMap.put("name", name);
+			userMap.put("surname", surname);
+			userMap.put("email", email);
+			userMap.put("imgurl", imgUrl);
+
+			String plainUserJson = null;
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				plainUserJson = mapper.writeValueAsString(userMap);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Crypter crypter = new Crypter();
+			String cryptedJson = null;
+			try {
+				cryptedJson = crypter.encrypt(plainUserJson);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			ClientResponse response = wr.path("/users").path("/auth").post(ClientResponse.class,cryptedJson);
+			
+			if(response.getStatus()==200){
+				String token = response.getEntity(String.class);
+				return Response.ok(BaseWebAppUri.concat("home")).cookie(new NewCookie("token",token)).build();
+			}
+			
+			//costruire parte else in cui, se il codice restituito dal server è 401 allora salva come cookie i dati con una chiave diversa,
+			//bisogna poi chiedere all'utente di inserire il nickname e provare la post, se fallisce chiedere di modificare i dati.
+			// l'inserimento del nickname potrebbe essere fatto in una richiesta asincrona AJAX.
+			
+			else {
+				String token = "";
+				return Response.ok(BaseWebAppUri.concat("landing")).cookie(new NewCookie("token",token)).build();
+			}
+			
+
+	}
+
 }
